94030ac0c3b43f2b34e0953ef6e1249c516bc682
yongzhegege <60562469+yongzhegege@users.noreply.github.com>
2026-03-03 13:40:29 +0800

    Delete App directory
    New app

--- App/build_protection.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cl /DUNICODE /D_UNICODE /EHsc Protection.cpp /link /out:Protection.exe Advapi32.lib
